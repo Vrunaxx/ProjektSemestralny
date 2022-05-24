98f76e64546299095be08f533a470d0d1fbea41a
Marcin Adamus <madamus2001@gmail.com>
2022-05-24 11:22:11 +0200

    Definition for DbContext

---
+++ ContextConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.SqlServer;
+using System.Text;
+
+namespace ProjektSemestralny
+{
+    public class ContextConfiguration : DbConfiguration
+    {
+        public ContextConfiguration()
+        {
+            SetExecutionStrategy("System.Data.SqlClient", () => new SqlAzureExecutionStrategy());
+        }
+    }
+}
--- ProjektSemestralnyDbContext.cs
+++ ProjektSemestralnyDbContext.cs
@@ -1,10 +1,20 @@
-﻿using System;
+﻿using ProjektSemestralny.Classes;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Text;
 
 namespace ProjektSemestralny
 {
-    internal class ProjektSemestralnyDbContext
+    [DbConfigurationType(typeof(ContextConfiguration))]
+    public class ProjektSemestralnyDbContext : DbContext
     {
+        public virtual IDbSet<Order> Orders { get; set; }
+        public virtual IDbSet<MovieProductId> MovieProductIds{ get; set; }
+        public virtual IDbSet<GameProductId> GameProductIds { get; set; }
+        public virtual IDbSet<BookProductId> BookProductIds { get; set; }
+        public virtual IDbSet<Movie> Movies { get; set; }
+        public virtual IDbSet<Game> Games { get; set; }
+        public virtual IDbSet<Book> Books { get; set; }
     }
 }

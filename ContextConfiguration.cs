d5dfa7c226855178686c66305d7853d985db16d2
Marcin Adamus <madamus2001@gmail.com>
2022-06-29 18:24:54 +0200

    The rest of Documentation

--- ContextConfiguration.cs
+++ ContextConfiguration.cs
@@ -3,8 +3,14 @@ using System.Data.Entity.SqlServer;
 
 namespace ProjektSemestralny
 {
+    /// <summary>
+    /// Logika za połączeniem do bazy danych
+    /// </summary>
     public class ContextConfiguration : DbConfiguration
     {
+        /// <summary>
+        /// Ustawia strategię egzekucji połączenia
+        /// </summary>
         public ContextConfiguration()
         {
             SetExecutionStrategy("System.Data.SqlClient", () => new SqlAzureExecutionStrategy());
--- ProjektSemestralnyDbContext.cs
+++ ProjektSemestralnyDbContext.cs
@@ -3,21 +3,45 @@ using System.Data.Entity;
 
 namespace ProjektSemestralny
 {
+    /// <summary>
+    /// Code First bazy danych oraz definicje dla tabel
+    /// </summary>
     [DbConfigurationType(typeof(ContextConfiguration))]
     public class ProjektSemestralnyDbContext : DbContext
     {
+        /// <summary>
+        /// Definicja tabeli Zamówień
+        /// </summary>
         public virtual IDbSet<Order> Orders { get; set; }
 
+        /// <summary>
+        /// Definicja tabeli łączącej Filmy i Zamówienia
+        /// </summary>
         public virtual IDbSet<MovieProductId> MovieProductIds{ get; set; }
 
+        /// <summary>
+        /// Definicja tabeli łączącej Gry i Zamówienia
+        /// </summary>
         public virtual IDbSet<GameProductId> GameProductIds { get; set; }
 
+        /// <summary>
+        /// Definicja tabeli łączącej Książki i Zamówienia
+        /// </summary>
         public virtual IDbSet<BookProductId> BookProductIds { get; set; }
 
+        /// <summary>
+        /// Definicja tabeli Filmy
+        /// </summary>
         public virtual IDbSet<Movie> Movies { get; set; }
 
+        /// <summary>
+        /// Definicja tabeli Gry
+        /// </summary>
         public virtual IDbSet<Game> Games { get; set; }
 
+        /// <summary>
+        /// Definicja tabeli Książek
+        /// </summary>
         public virtual IDbSet<Book> Books { get; set; }
     }
 }
